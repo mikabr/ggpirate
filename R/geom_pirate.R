ef0acbf4a705a80b29e97490187638ce1aad9e4b
Mika Braginsky <mika.br@gmail.com>
2017-10-04 15:11:23 -0400

    add alpha params, consistent param names #1

--- R/geom_pirate.R
+++ R/geom_pirate.R
@@ -2,16 +2,6 @@
 #' @importFrom dplyr "%>%"
 NULL
 
-modify_aes <- function(mapping, ...) {
-  if (is.null(mapping)) mapping <- aes()
-  utils::modifyList(mapping, ...)
-}
-
-GeomPointHollow <- ggproto(
-  "GeomPointHollow", GeomPoint,
-  default_aes = modify_aes(GeomPoint$default_aes, aes(shape = 1, size = 1))
-)
-
 GeomMeanLine <- ggproto(
   "GeomMeanLine", GeomCrossbar,
 
@@ -46,11 +36,7 @@ GeomCI <- ggproto(
                     width = params$width) %>%
       dplyr::left_join(data_entries, by = "group")
     GeomTile$setup_data(data_summary, params)
-  },
-
-  default_aes = modify_aes(GeomTile$default_aes,
-                           aes(alpha = 0.5, fill = "white", size = 0.5,
-                               colour = "darkgrey"))
+  }
 )
 
 GeomMeanBar <- ggproto(
@@ -67,21 +53,9 @@ GeomMeanBar <- ggproto(
       dplyr::mutate(ymin = y, ymax = y, width = params$width) %>%
       dplyr::left_join(data_entries, by = "group")
     GeomCol$setup_data(data_summary, params)
-  },
-
-  default_aes = modify_aes(GeomCol$default_aes, aes(alpha = 0.3))
-
-)
-
-GeomViolinBkg <- ggproto(
-  "GeomViolinBkg", GeomViolin,
-
-  draw_group = function(self, data, ...) {
-    data <- data %>% dplyr::mutate(colour = "darkgrey", fill = "transparent")
-    GeomViolin$draw_group(data, ...)
   }
-
 )
+
 
 #' Pirate plots
 #'
@@ -105,16 +79,22 @@ GeomViolinBkg <- ggproto(
 #' @param lines logical indicating whether to show mean lines
 #' @param cis logical indicating whether to show confidence intervals boxes
 #' @param violins logical indicating whether to show violins
-#' @param point_width Amount of horizontal jitter added to the locations of the
+#' @param width_points Amount of horizontal jitter added to the locations of the
 #'   points. Defaults to 10\% of the resolution of the data.
-#' @param bar_width Width of mean bars. Defaults to 90\% of the resolution of
+#' @param width_bars Width of mean bars. Defaults to 90\% of the resolution of
 #'   the data.
-#' @param line_width Width of mean lines. Defaults to 90\% of the resolution of
+#' @param width_lines Width of mean lines. Defaults to 90\% of the resolution of
 #'   the data.
-#' @param ci_width Width of confidence interval boxes. Defaults to 80\% of the
+#' @param width_cis Width of confidence interval boxes. Defaults to 80\% of the
 #'   resolution of the data.
-#' @param violin_width Width of violins. Defaults to 70\% of the resolution of
+#' @param width_violins Width of violins. Defaults to 70\% of the resolution of
 #'   the data.
+#' @param alpha_points Opacity of the points. Defaults to 1 (not transparent at
+#'   all).
+#' @param alpha_bars Opacity of the mean bars. Defaults to 0.3.
+#' @param alpha_cis Opacity of the confidence interval boxes. Defaults to 0.5.
+#' @param alpha_violins Opacity of the violins. Defaults to 0 (completely
+#'   transparent).
 #' @param fatten A multiplicative factor used to increase the size of the mean
 #'   lines.
 #' @param show.legend logical indicating whether this layer be included in the
@@ -134,11 +114,15 @@ geom_pirate <- function(mapping = NULL, data = NULL,
                         lines = TRUE,
                         cis = TRUE,
                         violins = TRUE,
-                        point_width = 0.1,
-                        bar_width = 0.9,
-                        line_width = 0.9,
-                        ci_width = 0.8,
-                        violin_width = 0.7,
+                        width_points = 0.1,
+                        width_bars = 0.9,
+                        width_lines = 0.9,
+                        width_cis = 0.8,
+                        width_violins = 0.7,
+                        alpha_points = 1,
+                        alpha_bars = 0.3,
+                        alpha_cis = 0.5,
+                        alpha_violins = 0,
                         trim = TRUE,         # geom_violin
                         scale = "area",      # geom_violin
                         fatten = 2.5,        # geom_crossbar
@@ -158,7 +142,8 @@ geom_pirate <- function(mapping = NULL, data = NULL,
       show.legend = show.legend,
       inherit.aes = inherit.aes,
       params = list(
-        width = bar_width,
+        width = width_bars,
+        alpha = alpha_bars,
         na.rm = na.rm,
         ...
       )
@@ -171,12 +156,15 @@ geom_pirate <- function(mapping = NULL, data = NULL,
       data = data,
       mapping = mapping,
       stat = "ydensity",
-      geom = GeomViolinBkg,
+      geom = GeomViolin,
       position = "dodge",
       show.legend = FALSE,
       inherit.aes = inherit.aes,
       params = list(
-        width = violin_width,
+        width = width_violins,
+        alpha = alpha_violins,
+        colour = "darkgrey",
+        fill = "white",
         trim = trim,
         scale = scale,
         na.rm = na.rm,
@@ -196,7 +184,11 @@ geom_pirate <- function(mapping = NULL, data = NULL,
       show.legend = FALSE,
       inherit.aes = inherit.aes,
       params = list(
-        width = ci_width,
+        width = width_cis,
+        alpha = alpha_cis,
+        colour = "darkgrey",
+        fill = "white",
+        size = 0.5,
         na.rm = na.rm,
         ...
       )
@@ -214,7 +206,7 @@ geom_pirate <- function(mapping = NULL, data = NULL,
       show.legend = FALSE,
       inherit.aes = inherit.aes,
       params = list(
-        width = line_width,
+        width = width_lines,
         fatten = fatten,
         na.rm = na.rm,
         ...
@@ -228,11 +220,14 @@ geom_pirate <- function(mapping = NULL, data = NULL,
       data = data,
       mapping = mapping,
       stat = "identity",
-      geom = GeomPointHollow,
-      position = position_jitter(width = point_width, height = 0),
+      geom = GeomPoint,
+      position = position_jitter(width = width_points, height = 0),
       show.legend = FALSE,
       inherit.aes = inherit.aes,
       params = list(
+        alpha = alpha_points,
+        shape = 1,
+        size = 1,
         na.rm = na.rm,
         ...
       )
